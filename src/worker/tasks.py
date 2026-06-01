67d57edb3785c6547e43154f6009bb347347cd8a
mklyazhev <janbrg@yandex.ru>
2026-06-01 21:06:36 +0300

    [feat]:[news-enrichment-backend] Добавил воркер

---
+++ src/worker/tasks.py
@@ -0,0 +1,21 @@
+import asyncio
+
+from celery.utils.log import get_task_logger
+
+from src.services.enrichment import run_enrichment_job
+from src.worker.celery_app import celery_app
+
+logger = get_task_logger(__name__)
+
+
+@celery_app.task(
+    bind=True,
+    autoretry_for=(RuntimeError,),
+    retry_backoff=True,
+    retry_jitter=True,
+    max_retries=3,
+    name="news_enrichment.run_enrichment_job",
+)
+def run_enrichment_job_task(self, job_id: int) -> None:
+    logger.info("Starting enrichment job %s", job_id)
+    asyncio.run(run_enrichment_job(job_id))
